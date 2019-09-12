fc4b15ba2c09424da331ab72592a4276a48c14ef
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-09-13 00:24:32 +0200

    intermediate revision - updated dependencies and added some data annotation attribute TODO note

--- src/test/java/com/github/sergueik/testng/ExcelProviderTest.java
+++ src/test/java/com/github/sergueik/testng/ExcelProviderTest.java
@@ -4,17 +4,28 @@ package com.github.sergueik.testng;
  */
 
 import org.testng.annotations.Test;
+import java.io.File;
+import java.io.IOException;
 
 // NOTE: needed to switch to hamcrest-all.jar and Matchers 
 // just for resolving method 'containsInAnyOrder'
 // then eclipse periodically forgets to include it 
 public class ExcelProviderTest extends CommonTest {
+// TODO:  document
+//      element value must be a constant expression
+// cannot use class variables in annotation value:
+	private static final String osName = System.getProperty("os.name").toLowerCase();
+	private static final String dataFilePath = (osName.startsWith("windows")) ? "${USERPROFILE}\\Desktop"
+			: "${HOME}/Desktop";
 
+	// cannot use File.separator in annotation value:
+//	@Test(groups = { "excel" }, enabled = true, singleThreaded = true, threadPoolSize = 1, invocationCount = 1, description = "# of articless for specific keyword", dataProvider = "Excel 2003", dataProviderClass = ExcelParametersProvider.class)
+//	@DataFileParameters(name = "data_2003.xls", path = "${USERPROFILE}" + File.separator  + "Desktop" , sheetName = "Employee Data")
 	@Test(groups = {
 			"excel" }, enabled = true, singleThreaded = true, threadPoolSize = 1, invocationCount = 1, description = "# of articless for specific keyword", dataProvider = "Excel 2003", dataProviderClass = ExcelParametersProvider.class)
 	@DataFileParameters(name = "data_2003.xls", path = "${USERPROFILE}\\Desktop", sheetName = "Employee Data")
-	public void testWithExcel2003(double rowNum, String searchKeyword,
-			double expectedCount) throws InterruptedException {
+	public void testWithExcel2003(double rowNum, String searchKeyword, double expectedCount)
+			throws InterruptedException {
 		dataTest(searchKeyword, expectedCount);
 
 	}
@@ -22,8 +33,8 @@ public class ExcelProviderTest extends CommonTest {
 	@Test(groups = {
 			"excel" }, enabled = true, singleThreaded = true, threadPoolSize = 1, invocationCount = 1, description = "# of articless for specific keyword", dataProvider = "Excel 2007", dataProviderClass = ExcelParametersProvider.class)
 	@DataFileParameters(name = "data_2007.xlsx", path = ".", sheetName = "Employee Data", debug = true)
-	public void testWithExcel2007(double rowNum, String searchKeyword,
-			double expectedCount) throws InterruptedException {
+	public void testWithExcel2007(double rowNum, String searchKeyword, double expectedCount)
+			throws InterruptedException {
 		dataTest(searchKeyword, expectedCount);
 	}
 
@@ -32,14 +43,17 @@ public class ExcelProviderTest extends CommonTest {
 	 * dd584d3341e3fe782d2d4c2d3ed6a8b77a6c21d2
 	 */
 	/*
-	@WindowsOnly
-	@Test(groups = {
-	"excel" }, enabled = true, singleThreaded = true, threadPoolSize = 1, invocationCount = 1, description = "# of articless for specific keyword", dataProvider = "Excel 2007", dataProviderClass = ExcelParametersProvider.class)
-	@DataFileParameters(name = "data_2007.xlsx", path = ".", sheetName = "Employee Data", debug = true)
-	public void testDecoratedWithDataProvererAndConditionalMethodAnnotation(
-			double rowNum, String searchKeyword, double expectedCount)
-			throws InterruptedException {
-		dataTest(searchKeyword, expectedCount);
-	}
-	*/
+	 * @WindowsOnly
+	 * 
+	 * @Test(groups = { "excel" }, enabled = true, singleThreaded = true,
+	 * threadPoolSize = 1, invocationCount = 1, description =
+	 * "# of articless for specific keyword", dataProvider = "Excel 2007",
+	 * dataProviderClass = ExcelParametersProvider.class)
+	 * 
+	 * @DataFileParameters(name = "data_2007.xlsx", path = ".", sheetName =
+	 * "Employee Data", debug = true) public void
+	 * testDecoratedWithDataProvererAndConditionalMethodAnnotation( double rowNum,
+	 * String searchKeyword, double expectedCount) throws InterruptedException {
+	 * dataTest(searchKeyword, expectedCount); }
+	 */
 }
