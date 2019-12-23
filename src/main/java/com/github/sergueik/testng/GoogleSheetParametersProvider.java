0687411260a7cf9a6e4433c1686dfbbdd7a517dd
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2019-12-23 16:03:32 -0500

    intermediate revision - misc cleanup of unused attributes and documentation update

--- src/main/java/com/github/sergueik/testng/GoogleSheetParametersProvider.java
+++ src/main/java/com/github/sergueik/testng/GoogleSheetParametersProvider.java
@@ -19,7 +19,6 @@ public class GoogleSheetParametersProvider {
 
 	private static Utils utils = Utils.getInstance();
 	private static boolean debug = false;
-	private static boolean loadEmptyColumns = false;
 	// store the spreadsheet id through the path parameter:
 	// with Google Sheet it has no verbatim meaning
 	private static String spreadsheetId = null;
@@ -27,17 +26,13 @@ public class GoogleSheetParametersProvider {
 	private static String applicationName = "Google Sheets Example";
 	private static String sheetName = "*";
 	private static String secretFilePath = null;
-	private static String columnNames = "*";
-	private final static String testEnvironment = (System.getenv("TEST_ENVIRONMENT") != null)
-			? System.getenv("TEST_ENVIRONMENT")
-			: "";
-	private static String controlColumn = null;
-	private static String withValue = null;
 
 	@DataProvider(parallel = true, name = "Google Spreadsheet")
-	public static Object[][] createDataFromGoogleSpreadsheet(final ITestContext context, final Method method) {
+	public static Object[][] createDataFromGoogleSpreadsheet(
+			final ITestContext context, final Method method) {
 
-		DataFileParameters parameters = method.getAnnotation(DataFileParameters.class);
+		DataFileParameters parameters = method
+				.getAnnotation(DataFileParameters.class);
 		if (parameters != null) {
 			applicationName = parameters.name();
 			spreadsheetId = parameters.path();
@@ -45,25 +40,16 @@ public class GoogleSheetParametersProvider {
 			debug = parameters.debug();
 			secretFilePath = parameters.secretFilePath();
 		} else {
-			throw new RuntimeException("Missing / invalid DataFileParameters annotation");
+			throw new RuntimeException(
+					"Missing / invalid DataFileParameters annotation");
 		}
 		utils.setApplicationName(applicationName);
 		utils.setSheetName(sheetName);
-		utils.setColumnNames(columnNames);
-		loadEmptyColumns = parameters.loadEmptyColumns();
-		controlColumn = parameters.controlColumn();
-		if (!controlColumn.isEmpty()) {
-			utils.setControlColumn(controlColumn);
-		}
-		withValue = parameters.withValue();
-		if (!withValue.isEmpty()) {
-			utils.setWithValue(withValue);
-		}
-
-		utils.setLoadEmptyColumns(loadEmptyColumns);
 		utils.setDebug(debug);
+		utils.setSecretFilePath(secretFilePath);
 
-		List<Object[]> result = utils.createDataFromGoogleSpreadsheet(spreadsheetId, sheetName);
+		List<Object[]> result = utils.createDataFromGoogleSpreadsheet(spreadsheetId,
+				sheetName);
 
 		Object[][] resultArray = new Object[result.size()][];
 		result.toArray(resultArray);
--- src/main/java/com/github/sergueik/testng/Utils.java
+++ src/main/java/com/github/sergueik/testng/Utils.java
@@ -1,4 +1,7 @@
 package com.github.sergueik.testng;
+/**
+ * Copyright 2017,2019 Serguei Kouzmine
+ */
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -84,8 +87,9 @@ public class Utils {
 		this.sheetName = data;
 	}
 
-	// will use name path
-	private String secretFilePath = null;
+	private String secretFilePath = Paths.get(System.getProperty("user.home"))
+			.resolve(".secret").resolve("client_secret.json").toAbsolutePath()
+			.toString();
 
 	public void setSecretFilePath(String data) {
 		this.secretFilePath = data;
@@ -131,7 +135,8 @@ public class Utils {
 	}
 
 	private static String osName = getOsName();
-	private static final String homeDir = System.getenv((osName.startsWith("windows")) ? "USERPROFILE" : "HOME");
+	private static final String homeDir = System
+			.getenv((osName.startsWith("windows")) ? "USERPROFILE" : "HOME");
 
 	public static String getOsName() {
 		if (osName == null) {
@@ -148,16 +153,20 @@ public class Utils {
 			return null;
 		}
 		// NOTE: ignoring $HOMEDRIVE, $HOMEPATH on Windows
-		Matcher matcher = Pattern.compile("\\$(?:\\{(\\w+)\\}|(\\w+))").matcher(
-				input.replaceAll("(?:HOME|HOMEDIR|USERPROFILE)", osName.equals("windows") ? "USERPROFILE" : "HOME"));
+		Matcher matcher = Pattern.compile("\\$(?:\\{(\\w+)\\}|(\\w+))")
+				.matcher(input.replaceAll("(?:HOME|HOMEDIR|USERPROFILE)",
+						osName.equals("windows") ? "USERPROFILE" : "HOME"));
 		StringBuffer stringBuffer = new StringBuffer();
 		while (matcher.find()) {
-			String envVarName = null == matcher.group(1) ? matcher.group(2) : matcher.group(1);
+			String envVarName = null == matcher.group(1) ? matcher.group(2)
+					: matcher.group(1);
 			String envVarValue = getPropertyEnv(envVarName, null);
-			matcher.appendReplacement(stringBuffer, null == envVarValue ? "" : envVarValue.replace("\\", "\\\\"));
+			matcher.appendReplacement(stringBuffer,
+					null == envVarValue ? "" : envVarValue.replace("\\", "\\\\"));
 		}
 		matcher.appendTail(stringBuffer);
-		return stringBuffer.toString().replaceAll("(?:\\\\|/)", (File.separator.indexOf("\\") > -1) ? "\\\\" : "/");
+		return stringBuffer.toString().replaceAll("(?:\\\\|/)",
+				(File.separator.indexOf("\\") > -1) ? "\\\\" : "/");
 	}
 
 	// origin:
@@ -174,16 +183,20 @@ public class Utils {
 	}
 
 	@SuppressWarnings("rawtypes")
-	public List<Object[]> createDataFromOpenOfficeSpreadsheet(SpreadSheet spreadSheet) {
+	public List<Object[]> createDataFromOpenOfficeSpreadsheet(
+			SpreadSheet spreadSheet) {
 		HashMap<String, String> columns = new HashMap<>();
 		List<Object[]> result = new LinkedList<>();
 		if (debug) {
-			System.err.println("Opening " + (sheetName.isEmpty() ? "first sheet" : sheetName));
+			System.err.println(
+					"Opening " + (sheetName.isEmpty() ? "first sheet" : sheetName));
 		}
 
-		Sheet sheet = (sheetName.isEmpty()) ? spreadSheet.getFirstSheet() : spreadSheet.getSheet(sheetName);
+		Sheet sheet = (sheetName.isEmpty()) ? spreadSheet.getFirstSheet()
+				: spreadSheet.getSheet(sheetName);
 		if (debug) {
-			System.err.println("Reading Open Office Spreadsheet : " + sheet.getName());
+			System.err
+					.println("Reading Open Office Spreadsheet : " + sheet.getName());
 		}
 		int columnCount = sheet.getColumnCount();
 		int rowCount = sheet.getRowCount();
@@ -194,20 +207,23 @@ public class Utils {
 			System.err.println("Determine control column index for " + controlColumn);
 		}
 		for (int columnIndex = 0; columnIndex < columnCount; columnIndex++) {
-			String columnHeader = sheet.getImmutableCellAt(columnIndex, 0).getValue().toString();
+			String columnHeader = sheet.getImmutableCellAt(columnIndex, 0).getValue()
+					.toString();
 			if (StringUtils.isBlank(columnHeader)) {
 				break;
 			}
 			String columnName = CellReference.convertNumToColString(columnIndex);
 			if (debug) {
-				System.err.println("Processing column # " + columnIndex + " row 0 " + columnName + " " + columnHeader);
+				System.err.println("Processing column # " + columnIndex + " row 0 "
+						+ columnName + " " + columnHeader);
 			}
-			if (controlColumn == null || controlColumn.isEmpty() || !controlColumn.equals(columnHeader)) {
+			if (controlColumn == null || controlColumn.isEmpty()
+					|| !controlColumn.equals(columnHeader)) {
 				columns.put(columnName, columnHeader);
 			} else {
 				controlColumnIndex = columnIndex;
-				System.err.println("Determined control column index " + columnIndex + " and " + columnName + " for "
-						+ columnHeader);
+				System.err.println("Determined control column index " + columnIndex
+						+ " and " + columnName + " for " + columnHeader);
 			}
 		}
 		// NOTE: often there may be no ranges defined
@@ -220,8 +236,8 @@ public class Utils {
 			}
 		}
 		// isCellBlank has protected access in Table
-		for (int rowIndex = 1; rowIndex < rowCount
-				&& StringUtils.isNotBlank(sheet.getImmutableCellAt(0, rowIndex).getValue().toString()); rowIndex++) {
+		for (int rowIndex = 1; rowIndex < rowCount && StringUtils.isNotBlank(sheet
+				.getImmutableCellAt(0, rowIndex).getValue().toString()); rowIndex++) {
 			List<Object> resultRow = new LinkedList<>();
 			if (controlColumnIndex != -1) {
 				controlCell = sheet.getImmutableCellAt(controlColumnIndex, rowIndex);
@@ -235,7 +251,8 @@ public class Utils {
 					continue;
 				}
 			}
-			for (int columnIndex = 0; columnIndex < columns.keySet().size(); columnIndex++) {
+			for (int columnIndex = 0; columnIndex < columns.keySet()
+					.size(); columnIndex++) {
 
 				String columnName = CellReference.convertNumToColString(columnIndex);
 				if (columns.containsKey(columnName)) {
@@ -262,12 +279,13 @@ public class Utils {
 							// https://docs.microsoft.com/en-us/office/vba/api/excel.range.address
 							// in org.jopendocument.dom.spreadsheet
 							// the getRowSpanned, getColumnsSpanned returning 1,1
-							System.err.println(String.format("Cell address: row: %d col: %d", cell.getRowsSpanned(),
-									cell.getColumnsSpanned()));
+							System.err.println(String.format("Cell address: row: %d col: %d",
+									cell.getRowsSpanned(), cell.getColumnsSpanned()));
 						}
 						if (debug) {
-							System.err.println(String.format("Cell value: \"%s\" class \"%s\"", cellValue.toString(),
-									cellValue.getClass().getName()));
+							System.err
+									.println(String.format("Cell value: \"%s\" class \"%s\"",
+											cellValue.toString(), cellValue.getClass().getName()));
 						}
 						resultRow.add(cellValue);
 					} else {
@@ -285,7 +303,8 @@ public class Utils {
 		return result;
 	}
 
-	public List<Object[]> createDataFromOpenOfficeSpreadsheet(InputStream inputStream) {
+	public List<Object[]> createDataFromOpenOfficeSpreadsheet(
+			InputStream inputStream) {
 		List<Object[]> result = new LinkedList<>();
 		try {
 			// https://www.programcreek.com/java-api-examples/index.php?api=org.jopendocument.dom.spreadsheet.Sheet
@@ -320,7 +339,8 @@ public class Utils {
 
 		Iterator<org.apache.poi.ss.usermodel.Cell> cells;
 		Map<String, String> columnHeaders = new HashMap<>();
-		HSSFSheet sheet = (sheetName.isEmpty()) ? workBook.getSheetAt(0) : workBook.getSheet(sheetName);
+		HSSFSheet sheet = (sheetName.isEmpty()) ? workBook.getSheetAt(0)
+				: workBook.getSheet(sheetName);
 		if (debug) {
 			System.err.println("Reading Excel 2003 sheet : " + sheet.getSheetName());
 		}
@@ -336,10 +356,12 @@ public class Utils {
 					cell = (HSSFCell) cells.next();
 					int columnIndex = cell.getColumnIndex();
 					String columnHeader = cell.getStringCellValue();
-					String columnName = CellReference.convertNumToColString(cell.getColumnIndex());
+					String columnName = CellReference
+							.convertNumToColString(cell.getColumnIndex());
 					columnHeaders.put(columnName, columnHeader);
 					if (debug) {
-						System.err.println(columnIndex + " = " + columnName + " " + columnHeader);
+						System.err
+								.println(columnIndex + " = " + columnName + " " + columnHeader);
 					}
 				}
 				// skip the header
@@ -351,7 +373,8 @@ public class Utils {
 				List<Object> resultRow = new LinkedList<>();
 				if (loadEmptyColumns) {
 					// fill the Array with nulls
-					IntStream.range(0, columnHeaders.keySet().size()).forEach(o -> resultRow.add(null));
+					IntStream.range(0, columnHeaders.keySet().size())
+							.forEach(o -> resultRow.add(null));
 					// inject sparsely defined columns
 					while (cells.hasNext()) {
 						cell = (HSSFCell) cells.next();
@@ -359,10 +382,12 @@ public class Utils {
 							Object cellValue = safeUserModeCellValue(cell);
 							if (debug) {
 								try {
-									System.err.println(String.format("Loading Cell[%d] = %s %s", cell.getColumnIndex(),
-											cellValue.toString(), cellValue.getClass()));
+									System.err.println(String.format("Loading Cell[%d] = %s %s",
+											cell.getColumnIndex(), cellValue.toString(),
+											cellValue.getClass()));
 								} catch (NullPointerException e) {
-									System.err.println("Exception loading cell " + cell.getColumnIndex());
+									System.err.println(
+											"Exception loading cell " + cell.getColumnIndex());
 								}
 							}
 							resultRow.set(cell.getColumnIndex(), cellValue);
@@ -377,8 +402,9 @@ public class Utils {
 									cell.getAddress().getRow(), cell.getAddress().getColumn()));
 						}
 						if (debug) {
-							System.err.println(String.format("Cell value: \"%s\" class: \"%s\"", cellValue.toString(),
-									cellValue.getClass().getName()));
+							System.err
+									.println(String.format("Cell value: \"%s\" class: \"%s\"",
+											cellValue.toString(), cellValue.getClass().getName()));
 						}
 						resultRow.add(cellValue);
 					}
@@ -435,7 +461,8 @@ public class Utils {
 	public List<Object[]> createDataFromExcel2007(XSSFWorkbook workBook) {
 		List<Object[]> result = new LinkedList<>();
 		Map<String, String> columns = new HashMap<>();
-		XSSFSheet sheet = (sheetName.isEmpty()) ? workBook.getSheetAt(0) : workBook.getSheet(sheetName);
+		XSSFSheet sheet = (sheetName.isEmpty()) ? workBook.getSheetAt(0)
+				: workBook.getSheet(sheetName);
 
 		Iterator<Row> rows = sheet.rowIterator();
 		Iterator<org.apache.poi.ss.usermodel.Cell> cells;
@@ -449,10 +476,12 @@ public class Utils {
 					cell = (XSSFCell) cells.next();
 					int columnIndex = cell.getColumnIndex();
 					String columnHeader = cell.getStringCellValue();
-					String columnName = CellReference.convertNumToColString(cell.getColumnIndex());
+					String columnName = CellReference
+							.convertNumToColString(cell.getColumnIndex());
 					columns.put(columnName, columnHeader);
 					if (debug) {
-						System.err.println(columnIndex + " = " + columnName + " " + columnHeader);
+						System.err
+								.println(columnIndex + " = " + columnName + " " + columnHeader);
 					}
 				}
 				// skip the header
@@ -463,7 +492,8 @@ public class Utils {
 			if (cells.hasNext()) {
 				if (loadEmptyColumns) {
 					// fill the Array with nulls
-					IntStream.range(0, columns.keySet().size()).forEach(o -> resultRow.add(null));
+					IntStream.range(0, columns.keySet().size())
+							.forEach(o -> resultRow.add(null));
 					// inject sparsely defined columns
 					while (cells.hasNext()) {
 						cell = (XSSFCell) cells.next();
@@ -471,12 +501,14 @@ public class Utils {
 						if (cell != null) {
 							Object cellValue = safeUserModeCellValue(cell);
 							if (debug) {
-								System.err.println(String.format("Cell address: row: %d col: %d",
-										cell.getAddress().getRow(), cell.getAddress().getColumn()));
+								System.err.println(String.format(
+										"Cell address: row: %d col: %d", cell.getAddress().getRow(),
+										cell.getAddress().getColumn()));
 							}
 							if (debug) {
-								System.err.println(String.format("Cell value: \"%s\" class: \"%s\"",
-										cellValue.toString(), cellValue.getClass().getName()));
+								System.err
+										.println(String.format("Cell value: \"%s\" class: \"%s\"",
+												cellValue.toString(), cellValue.getClass().getName()));
 							}
 							resultRow.add(cellValue);
 						}
@@ -488,12 +520,13 @@ public class Utils {
 						if (cell != null) {
 							Object cellValue = safeUserModeCellValue(cell);
 							if (debug) {
-								System.err.println(String.format("Cell address: row: %d col: %d",
-										cell.getAddress().getRow(), cell.getAddress().getColumn()));
+								System.err.println(String.format(
+										"Cell address: row: %d col: %d", cell.getAddress().getRow(),
+										cell.getAddress().getColumn()));
 							}
 							if (debug) {
-								System.err.println(String.format("Cell value: \"%s\" class: %s", cellValue.toString(),
-										cellValue.getClass().getName()));
+								System.err.println(String.format("Cell value: \"%s\" class: %s",
+										cellValue.toString(), cellValue.getClass().getName()));
 							}
 							resultRow.add(cellValue);
 						}
@@ -524,59 +557,6 @@ public class Utils {
 		return result;
 	}
 
-	public List<Object[]> createDataFromGoogleSpreadsheet(String spreadsheetId) {
-		return createDataFromGoogleSpreadsheet(spreadsheetId, "*");
-	}
-
-	// temporarily add to signature
-	public List<Object[]> createDataFromGoogleSpreadsheet(String spreadsheetId, String sheetName) {
-		// TODO: deal with unspecified sheetName
-		String range = String.format("%s!A2:Z", sheetName);
-		// A2:Z for value columns only
-		List<Object[]> result = new LinkedList<>();
-
-		try {
-			String secretFilePath = Paths.get(System.getProperty("user.home")).resolve(".secret")
-					.resolve("client_secret.json").toAbsolutePath().toString();
-			sheetsServiceUtil = SheetsServiceUtil.getInstance();
-			sheetsServiceUtil.setApplicationName(applicationName);
-			sheetsServiceUtil.setSecretFilePath(secretFilePath);
-			Sheets sheetsService = sheetsServiceUtil.getSheetsService();
-
-			ValueRange response = sheetsService.spreadsheets().values().get(spreadsheetId, range).execute();
-
-			List<List<Object>> resultRows = response.getValues();
-			assertThat(resultRows, notNullValue());
-			assertThat(resultRows.size() != 0, is(true));
-			if (debug) {
-				System.err.println("Got " + resultRows.size() + " result rows");
-			}
-			int row = 0;
-			for (List<Object> resultRow : resultRows) {
-				if (row == 0) {
-					System.err.println("Headers:");
-					Object[] resultArray = resultRow.toArray();
-					Integer numberOfCols = resultArray.length;
-					for (int col = 0; col != numberOfCols; col++) {
-						// TODO: column filter
-						if (debug) {
-							System.err.println(String.format("column[%d]: %s ", col, resultArray[col]));
-						}
-					}
-				} else {
-					if (debug) {
-						System.err.println("Got: " + resultRow);
-					}
-					result.add(resultRow.toArray());
-				}
-				row++;
-			}
-		} catch (IOException | GeneralSecurityException e) {
-			System.err.println("Exception (ignored): " + e.toString());
-		}
-		return result;
-	}
-
 	public List<Object[]> createDataFromExcel2007(InputStream inputStream) {
 
 		List<Object[]> result = new LinkedList<>();
@@ -599,7 +579,8 @@ public class Utils {
 	}
 
 	// Safe conversion of type Excel cell object to Object / String value
-	public static Object safeUserModeCellValue(org.apache.poi.ss.usermodel.Cell cell) {
+	public static Object safeUserModeCellValue(
+			org.apache.poi.ss.usermodel.Cell cell) {
 		if (cell == null) {
 			return null;
 		}
@@ -626,14 +607,16 @@ public class Utils {
 		case ERROR:
 			throw new RuntimeException("Cell has an error");
 		default:
-			throw new IllegalStateException("Cell type: " + type + " is not supported");
+			throw new IllegalStateException(
+					"Cell type: " + type + " is not supported");
 		}
 		return result;
 		// return (result == null) ? null : result.toString();
 	}
 
 	// https://www.jopendocument.org/docs/org/jopendocument/dom/ODValueType.html
-	public static Object safeOOCellValue(org.jopendocument.dom.spreadsheet.Cell<ODDocument> cell) {
+	public static Object safeOOCellValue(
+			org.jopendocument.dom.spreadsheet.Cell<ODDocument> cell) {
 		if (cell == null) {
 			return null;
 		}
@@ -656,6 +639,61 @@ public class Utils {
 			throw new IllegalStateException("Can't evaluate cell value");
 		}
 		// return (result == null) ? null : result.toString();
+		return result;
+	}
+
+	public List<Object[]> createDataFromGoogleSpreadsheet(String spreadsheetId) {
+		return createDataFromGoogleSpreadsheet(spreadsheetId, "*");
+	}
+
+	// temporarily add to signature
+	public List<Object[]> createDataFromGoogleSpreadsheet(String spreadsheetId,
+			String sheetName) {
+		// TODO: deal with unspecified sheetName
+		String range = String.format("%s!A1:Z", sheetName);
+		// A2:Z for value columns only
+		List<Object[]> result = new LinkedList<>();
+
+		try {
+
+			sheetsServiceUtil = SheetsServiceUtil.getInstance();
+			sheetsServiceUtil.setApplicationName(applicationName);
+			sheetsServiceUtil.setSecretFilePath(secretFilePath);
+			Sheets sheetsService = sheetsServiceUtil.getSheetsService();
+
+			ValueRange response = sheetsService.spreadsheets().values()
+					.get(spreadsheetId, range).execute();
+
+			List<List<Object>> resultRows = response.getValues();
+			assertThat(resultRows, notNullValue());
+			assertThat(resultRows.size() != 0, is(true));
+			if (debug) {
+				System.err.println("Got " + resultRows.size() + " result rows");
+			}
+			int row = 0;
+			for (List<Object> resultRow : resultRows) {
+				if (row == 0) {
+					System.err.println("Headers:");
+					Object[] resultArray = resultRow.toArray();
+					Integer numberOfCols = resultArray.length;
+					for (int col = 0; col != numberOfCols; col++) {
+						// TODO: column filter
+						if (debug) {
+							System.err.println(
+									String.format("Header[%d]: %s ", col, resultArray[col]));
+						}
+					}
+				} else {
+					if (debug) {
+						System.err.println("Got: " + resultRow);
+					}
+					result.add(resultRow.toArray());
+				}
+				row++;
+			}
+		} catch (IOException | GeneralSecurityException e) {
+			System.err.println("Exception (ignored): " + e.toString());
+		}
 		return result;
 	}
 
